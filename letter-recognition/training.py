263f2ef783b535ca3924500dcb899037bf702740
Per Thomas Hille <pth@embc.no>
2025-05-10 00:46:04 +0200

    The neural network is now configurable via paramters

--- letter-recognition/training.py
+++ letter-recognition/training.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import matplotlib.axes._axes as Axes
 import logging
 import warnings
-#from autils import *
 from utils import *
 import sys
 
@@ -19,7 +18,7 @@ tf.autograph.set_verbosity(0)
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 print(tf.__version__)
-NO_EPOCS = 30
+NO_EPOCS = 50
 
 X, y = load_data()
 m, n = X.shape
@@ -27,6 +26,8 @@ m, n = X.shape
 plot_random(8, 8, X, y, figsize=(8,8))
 
 layers_n = [25, 10, 15, 1]
+#layers_n = [25, 15, 1]
+
 model = Sequential( [tf.keras.Input(shape=(400,)),], name = "my_model")
 
 for l in layers_n:
@@ -34,20 +35,6 @@ for l in layers_n:
 
 model.summary()
 
-"""
-[layer1, layer2, layer3, layer4] = model.layers
-W1,b1 = layer1.get_weights()
-W2,b2 = layer2.get_weights()
-W3,b3 = layer3.get_weights()
-W4,b4 = layer4.get_weights()
-print(f"W1 shape = {W1.shape}, b1 shape = {b1.shape}")
-print(f"W2 shape = {W2.shape}, b2 shape = {b2.shape}")
-print(f"W3 shape = {W3.shape}, b3 shape = {b3.shape}")
-print(f"W4 shape = {W4.shape}, b4 shape = {b4.shape}")
-"""
-#print(model.layers[2].weights)
-    
-#sys.exit()
 model.compile( loss= BinaryCrossentropy(), optimizer= Adam(0.001),)
 model.fit(X,y,epochs=NO_EPOCS)
 
@@ -63,21 +50,18 @@ def dense_propagation_v(A_in, W, b, g):
     A_out = g(Z)
     return(A_out)
 
-    
-def inference_v2(X, W, b):
-    A1 = dense_propagation_v(X,  W[0], b[0], sigmoid)
-    A2 = dense_propagation_v(A1, W[1], b[1], sigmoid)
-    A3 = dense_propagation_v(A2, W[2], b[2], sigmoid)
-    print("A3 TYPE = ", type(A3))
-    A4 = dense_propagation_v(A3, W[3], b[3], sigmoid)
-    return(A4)
 
-def inference_v3(X, W, b):
-    A1 = dense_propagation_v(X,  W[0], b[0], sigmoid)
-    A2 = dense_propagation_v(A1, W[1], b[1], sigmoid)
-    A3 = dense_propagation_v(A2, W[2], b[2], sigmoid)
-    A4 = dense_propagation_v(A3, W[3], b[3], sigmoid)
-    return(A4)
+def inference_v2(X, W, b):
+    A = dense_propagation_v(X,  W[0], b[0], sigmoid)
+    length = len(W) - 1
+
+    i : int = 1
+    while length !=0:
+        A =  dense_propagation_v(A,  W[i], b[i], sigmoid)
+        length -= 1
+        i+= 1
+
+    return A
 
 
 W = []
@@ -90,7 +74,7 @@ for l in layers:
     b.append(b_l)
 
 prediction = inference_v2(X, W, b)
-
+#sys.exit()
 Yhat = (prediction >= 0.5).astype(int)
 print("predict a zero: ",Yhat[0], "predict a one: ", Yhat[500])
 
@@ -111,11 +95,11 @@ len_err = len(values)
 len_all = len(y)
 percent = 100*(len_err/len_all)
 
-print("{} out of {} images was miss classified ( {} %)".format(len_err, len_all, percent))
-
 
 for i in range(len(indexes)):
     print("index = {}, value = {}, yhat = {}".format(indexes[i], values[i], Yhat[ indexes[i] ] ))
+
+print("{} out of {} images was miss classified ( {} %)".format(len_err, len_all, percent))
 
 
 def plot_image(index):
