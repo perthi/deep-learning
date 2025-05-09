818adbfea21369ff450c24805340965c9535fa7f
Per Thomas Hille <pth@embc.no>
2025-05-09 16:05:52 +0200

    experimenting with training variables

--- letter-recognition/training.py
+++ letter-recognition/training.py
@@ -60,3 +60,147 @@ print("L1 params = ", L1_num_params, ", L2 params = ", L2_num_params, ",  L3 par
 # from public_tests import *
 # test_c1(model)
 
+[layer1, layer2, layer3] = model.layers
+W1,b1 = layer1.get_weights()
+W2,b2 = layer2.get_weights()
+W3,b3 = layer3.get_weights()
+print(f"W1 shape = {W1.shape}, b1 shape = {b1.shape}")
+print(f"W2 shape = {W2.shape}, b2 shape = {b2.shape}")
+print(f"W3 shape = {W3.shape}, b3 shape = {b3.shape}")
+
+
+print(model.layers[2].weights)
+
+model.compile(
+    loss=tf.keras.losses.BinaryCrossentropy(),
+    optimizer=tf.keras.optimizers.Adam(0.001),
+)
+
+model.fit(
+    X,y,
+    epochs=100
+)
+
+prediction = model.predict(X[0].reshape(1,400))  # a zero
+print(f" predicting a zero: {prediction}")
+prediction = model.predict(X[500].reshape(1,400))  # a one
+print(f" predicting a one:  {prediction}")
+
+
+if prediction >= 0.5:
+    yhat = 1
+else:
+    yhat = 0
+print(f"prediction after threshold: {yhat}")
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8,8, figsize=(8,8))
+fig.tight_layout(pad=0.1,rect=[0, 0.03, 1, 0.92]) #[left, bottom, right, top]
+
+for i,ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20,20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+    
+    # Predict using the Neural Network
+    prediction = model.predict(X[random_index].reshape(1,400))
+    if prediction >= 0.5:
+        yhat = 1
+    else:
+        yhat = 0
+    
+    # Display the label above the image
+    ax.set_title(f"{y[random_index,0]},{yhat}")
+    ax.set_axis_off()
+fig.suptitle("Label, yhat", fontsize=16)
+
+#plt.show()
+
+x = X[0].reshape(-1,1)         # column vector (400,1)
+z1 = np.matmul(x.T,W1) + b1    # (1,400)(400,25) = (1,25)
+a1 = sigmoid(z1)
+print(a1.shape)
+
+def my_dense_v(A_in, W, b, g):
+    AT = A_in
+    Z = np.matmul(AT,W) + b
+    A_out = g(Z)
+    return(A_out)
+
+
+X_tst = 0.1*np.arange(1,9,1).reshape(4,2) # (4 examples, 2 features)
+W_tst = 0.1*np.arange(1,7,1).reshape(2,3) # (2 input features, 3 output features)
+b_tst = 0.1*np.arange(1,4,1).reshape(1,3) # (1,3 features)
+A_tst = my_dense_v(X_tst, W_tst, b_tst, sigmoid)
+print(A_tst)
+
+from public_tests import *
+
+test_c3(my_dense_v)
+
+
+def my_sequential_v(X, W1, b1, W2, b2, W3, b3):
+    A1 = my_dense_v(X,  W1, b1, sigmoid)
+    A2 = my_dense_v(A1, W2, b2, sigmoid)
+    A3 = my_dense_v(A2, W3, b3, sigmoid)
+    return(A3)
+
+W1_tmp,b1_tmp = layer1.get_weights()
+W2_tmp,b2_tmp = layer2.get_weights()
+W3_tmp,b3_tmp = layer3.get_weights()
+
+
+Prediction = my_sequential_v(X, W1_tmp, b1_tmp, W2_tmp, b2_tmp, W3_tmp, b3_tmp )
+Prediction.shape
+
+Yhat = (Prediction >= 0.5).astype(int)
+print("predict a zero: ",Yhat[0], "predict a one: ", Yhat[500])
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+# You do not need to modify anything in this cell
+
+m, n = X.shape
+
+fig, axes = plt.subplots(8, 8, figsize=(8, 8))
+fig.tight_layout(pad=0.1, rect=[0, 0.03, 1, 0.92]) #[left, bottom, right, top]
+
+for i, ax in enumerate(axes.flat):
+    # Select random indices
+    random_index = np.random.randint(m)
+    
+    # Select rows corresponding to the random indices and
+    # reshape the image
+    X_random_reshaped = X[random_index].reshape((20, 20)).T
+    
+    # Display the image
+    ax.imshow(X_random_reshaped, cmap='gray')
+   
+    # Display the label above the image
+    ax.set_title(f"{y[random_index,0]}, {Yhat[random_index, 0]}")
+    ax.set_axis_off() 
+fig.suptitle("Label, Yhat", fontsize=16)
+#plt.show()
+
+fig = plt.figure(figsize=(1, 1))
+errors = np.where(y != Yhat)
+print("yhat:len = %d", len(Yhat))
+print("y:len = %d", len(y))
+
+#random_index = errors[0][0]
+#X_random_reshaped = X[random_index].reshape((20, 20)).T
+#plt.imshow(X_random_reshaped, cmap='gray')
+#plt.title(f"{y[random_index,0]}, {Yhat[random_index, 0]}")
+#plt.axis('off')
+#plt.show()
