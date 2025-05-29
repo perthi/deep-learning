c05e50f367384de2becb19f7ca0f8e858475fef6
Per Thomas Hille <pth@embc.no>
2025-05-29 03:44:14 +0200

    Documentation + dedciated file for config paramters

---
+++ letter-recognition/config.py
@@ -0,0 +1,5 @@
+
+
+NO_EPOCS = 50  # Number of training passes
+LAYERS = [25, 15,1] #Number  of nodes i the various neural network layers
+TEST_SIZE_FRACTION = 0.20 # The fraction of the data used for validation, muts be in the range [0,1]
